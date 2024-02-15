a9afe62ca5ac94ef378c48a663d48b672303a6ec
Abel  Titulaciòn2 <achingo@istrfa.edu.pe>
2024-02-14 19:22:35 -0500

    subiendo controlladores

--- src/main/java/com/istrfa/sistema/controllers/ClienteController.java
+++ src/main/java/com/istrfa/sistema/controllers/ClienteController.java
@@ -1,13 +1,50 @@
 package com.istrfa.sistema.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.istrfa.sistema.controllers.dto.DTOCliente;
+import com.istrfa.sistema.services.ClienteService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/cliente")
+@Slf4j
 public class ClienteController {
 
-    @GetMapping(value="saludo")
-    public String listar(){
-      return "Hola";
+    @Autowired
+    ClienteService clientesServices;
+
+    @GetMapping()
+    public ResponseEntity<List<DTOCliente>> listar(@RequestBody DTOCliente dto) {
+        List<DTOCliente> dato = clientesServices.listar(dto);
+        return new ResponseEntity<List<DTOCliente>>(dato, HttpStatus.OK);
+
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> guardar(@RequestBody DTOCliente dto) {
+        clientesServices.guardar(dto);
+        return new ResponseEntity<>("Registro guardado correctamente", HttpStatus.OK);
+
+    }
+
+    @PutMapping()
+    public ResponseEntity<String> actualizar(@RequestBody DTOCliente dto, @PathVariable(name = "id") UUID id) {
+        clientesServices.actualizar(dto, id);
+        return new ResponseEntity<>("Registro Actualizado correctamente", HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> eliminar(@PathVariable(name = "id") UUID id) {
+        clientesServices.eliminar(id);
+        return new ResponseEntity<>("Registro Eliminado correctamente", HttpStatus.OK);
+
     }
 }
+
